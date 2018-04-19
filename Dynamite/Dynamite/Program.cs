1b862b4a0e34562f5eeda642d875ab746c166bb3
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-19 14:38:40 +0100

    Added the bot interface

--- Dynamite/Dynamite/Program.cs
+++ Dynamite/Dynamite/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿                        using BotInterface.Bot;
+                        using BotInterface.Game;
 
-namespace Dynamite
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+                        namespace ExampleBot
+                        {
+                           public class ExampleBot : IBot
+                           {
+                               public Move MakeMove(Gamestate gamestate)
+                                {
+                                  return Move.P;
+                                 }
+                          }
+                        }
